51c24b09d0bb942213c9abcf4a59311d8c06b95f
Abdul Azim Imtiaz <146334286+AzimImtiaz@users.noreply.github.com>
2025-01-18 14:59:42 +0600

    Update Print Name.cpp

--- Print Name.cpp
+++ Print Name.cpp
@@ -1,10 +1,8 @@
 #include<iostream>
 using namespace std;
 int main(){
-
 cout<<"Name: Azim Imtiaz"; << endl;
-cout<<"Father's Name: Abdul Mannan"; << endl;
+cout<<"Father's Name: Abdul Mannan";<<endl;
 cout<<"Mother's Name: Rehana Begum";
-
 return 0;
 }
